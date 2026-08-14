2dc53d07c76a0fe32f0bfeb36ebaf89f38b6fbf3
rampawar123 <rampawaratoz174@gmail.com>
2026-08-14 15:04:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-// YASHWIN APP STORE
+const CACHE_NAME = "yashwin-app-store-v1";
 
-document.addEventListener("DOMContentLoaded", function () {
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.svg"
+];
 
-  console.log("YASHWIN APP STORE started");
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
 
-  // Open App button
-  const openAppButton = document.querySelector(".button");
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
 
-  if (openAppButton) {
-
-    openAppButton.addEventListener("click", function () {
-
-      console.log("VANSH CALCULATOR opened");
-
-    });
-
-  }
-
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
