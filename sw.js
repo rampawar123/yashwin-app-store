9c418e70c49ee864a46b907329dbec6d63afd314
rampawar123 <rampawaratoz174@gmail.com>
2026-08-14 17:25:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,48 @@
-const CACHE_NAME = "yashwin-app-store-v1";
+const CACHE_NAME = "yashwin-store-v1";
 
-const FILES_TO_CACHE = [
+const FILES = [
   "./",
   "./index.html",
+  "./calculator.html",
   "./style.css",
   "./script.js",
   "./manifest.json",
-  "./icon.svg"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      );
-    })
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
+
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response =>
+        response || fetch(event.request)
+      )
   );
+
 });
