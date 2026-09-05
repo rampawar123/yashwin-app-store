8a2fdd538395e1bd375b3870940dc6048abd87ca
rampawar123 <rampawaratoz174@gmail.com>
2026-09-05 22:59:37 +0530

    Add files via upload

---
+++ cric-yuva/server-config.js
@@ -0,0 +1,4 @@
+// Cric Yuva deployment configuration.
+// When the app and Node server are hosted together, keep this empty.
+// If frontend is hosted separately, set this to the public API server origin.
+window.CRIC_YUVA_API_BASE = "";
