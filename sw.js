8f7d3e07e4fb79949654e626c4724d6bc436db44
rampawar123 <rampawaratoz174@gmail.com>
2026-09-09 22:43:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,56 +1,32 @@
-const CACHE_NAME = "yashwin-app-store-v2";
+const CACHE = 'yashwin-store-v8';
+const SHELL = ['./','./index.html','./manifest.json','./icon-512.png'];
 
-const CORE_FILES = [
-  "./",
-  "./index.html",
-  "./calculator.html",
-  "./manifest.json",
-  "./style.css",
-  "./script.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(CORE_FILES))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
 
-  if (event.request.method !== "GET") {
+  // Always prefer the network for the store page so GitHub changes appear quickly.
+  if (req.mode === 'navigate' || url.pathname.endsWith('/index.html') || url.pathname === '/') {
+    event.respondWith(fetch(req, { cache: 'no-store' }).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./index.html'))));
     return;
   }
 
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-
-        const copy = response.clone();
-
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
-
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+    return res;
+  })));
 });
